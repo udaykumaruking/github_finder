ee3f0545f6f0142fa39db5aa2d78a96133afa0d9
Uday Kumar <udaykumaruking@gmail.com>
2020-10-09 16:02:13 +0530

    search bug fix on press textfieldd out opf scope

--- lib/screens/HomeBody.dart
+++ lib/screens/HomeBody.dart
@@ -5,24 +5,38 @@ class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: <Widget>[
-          sizedBoxH(10),
-          AppBarCustom(),
-          sizedBoxH(10),
-          Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10),
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        child: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            FocusScope.of(context).requestFocus(
+              new FocusNode(),
+            );
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(0),
             child: Column(
               children: <Widget>[
-                HeaderText(),
                 sizedBoxH(10),
-                RichTextWidget(),
-                sizedBoxH(15),
-                SearchBar(),
+                AppBarCustom(),
+                sizedBoxH(10),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: Column(
+                    children: <Widget>[
+                      HeaderText(),
+                      sizedBoxH(10),
+                      RichTextWidget(),
+                      sizedBoxH(15),
+                      SearchBar(),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
